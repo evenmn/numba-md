2d29768eff9acc7ea8fc7581a0d85cd85ef36c69
evenmn <evenmn@fys.uio.no>
2021-02-07 01:05:26 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mdsolver',
+      version='0.1',
+      description='Molecular dynamics simulator written in Python and accelerated by the Numba JIT compiler',
+      url='http://github.com/evenmn/numba-md',
+      author='Even M. Nordhagen',
+      author_email='even.nordhagen@live.no',
+      license='MIT',
+      packages=['mdsolver'],
+      zip_safe=False)
